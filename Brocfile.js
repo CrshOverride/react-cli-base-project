436e9f41d8a67f5e89944901c64ce244314ba44d
Justin Niessner <justin.niessner@adroll.com>
2015-12-16 20:39:14 -0500

    Now with clean sourcemaps!

--- Brocfile.js
+++ Brocfile.js
@@ -1,11 +1,11 @@
 'use strict';
 
-let babel                       = require('broccoli-babel-transpiler'),
-    broccoliSource              = require('broccoli-source'),
+let broccoliSource              = require('broccoli-source'),
     mergeTrees                  = require('broccoli-merge-trees'),
     concat                      = require('broccoli-concat'),
     funnel                      = require('broccoli-funnel'),
     browserify                  = require('broccoli-fast-browserify'),
+    babelify                    = require('babelify'),
     omit                        = require('lodash.omit'),
     merge                       = require('lodash.merge'),
     npmJson                     = require('./package.json'),
@@ -28,15 +28,23 @@ class ReactApp {
     }
 
     appJavascript(thirdPartyDeps) {
-        let appJs = babel('app', babelOptions);
+        let appJs = funnel('app', {
+            include: ['**/*.jsx', '**/*.js']
+        });
 
         appJs = browserify(appJs, {
             browserify: {
-                debug: true
+                debug: true,
+                extensions: ['.js', '.jsx']
             },
             bundles: {
                 'scripts/app.js': {
-                    entryPoints: ['index.js'],
+                    transform: [
+                        [babelify, {
+                            presets: ['es2015', 'react']
+                        }]
+                    ],
+                    entryPoints: ['index.jsx'],
                     externals: thirdPartyDeps
                 }
             }
